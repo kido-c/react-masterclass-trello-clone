82a5955bfe0a05a0784e52f6a644c04fef7b15ac
이해-도기석 <2hae-kido@ihae-dogiseog-ui-MacBookPro.local>
2022-03-10 19:13:34 +0900

    start trello clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { minutesState } from './atoms';
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        onChange={onMinutesChange}
+        value={minutes}
+        type="number"
+        placeholder="minutes"
+      />
+      <input type="number" placeholder="Hours" />
     </div>
   );
 }
